031e44aa3dde72fd5f74d191b8140bb4217a2269
bigocb <bobby-cloutier@gmail.com>
2026-05-31 11:30:35 -0400

    temporary save to clean slate

--- apps/api/db/migrations/0013_terminology_sweep.sql
+++ apps/api/db/migrations/0013_terminology_sweep.sql
@@ -5,12 +5,12 @@
 
 ALTER TABLE judgments RENAME COLUMN difficulty TO commitment;
 
--- Fix constraints in the stunts table
-ALTER TABLE stunts DROP CONSTRAINT IF EXISTS stunts_status_check;
-ALTER TABLE stunts DROP CONSTRAINT IF EXISTS stunts_check;
+-- Fix constraints in the jumps table
+ALTER TABLE jumps DROP CONSTRAINT IF EXISTS jumps_status_check;
+ALTER TABLE jumps DROP CONSTRAINT IF EXISTS jumps_check;
 
-ALTER TABLE stunts ADD CONSTRAINT stunts_status_check 
+ALTER TABLE jumps ADD CONSTRAINT jumps_status_check 
     CHECK (status IN ('Idea', 'Planned Jump', 'Performed Jump', 'Judged Jump', 'Unwitnessed Jump', 'Disqualified Jump', 'Removed Jump'));
 
-ALTER TABLE stunts ADD CONSTRAINT stunts_check 
+ALTER TABLE jumps ADD CONSTRAINT jumps_check 
     CHECK ((season_id IS NULL) OR (status IN ('Planned Jump', 'Performed Jump', 'Judged Jump', 'Unwitnessed Jump', 'Disqualified Jump', 'Removed Jump')));
