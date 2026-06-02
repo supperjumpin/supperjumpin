28d68a9975a875e676242bd1e18c563b8667c263
Ben Turney <btatvt11@gmail.com>
2026-06-01 21:27:38 -0400

    fix(MemoryStore): set GracePeriodExpiresAt in ClaimAndAdvance to match PostgresStore

--- apps/api/internal/httpapi/memory_store.go
+++ apps/api/internal/httpapi/memory_store.go
@@ -567,6 +567,7 @@ func (s *MemoryStore) ClaimAndAdvance(ctx context.Context, authorizationID, jump
 
 	jump := s.jumps[jumpID]
 	jump.Status = "Performed Jump"
+	jump.GracePeriodExpiresAt = s.now().Add(10 * time.Minute).UTC()
 	s.jumps[jump.ID] = jump
 
 	evidenceID := stableID("evidence", jumpID+":"+authorizationID)
